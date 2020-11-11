04c92d0240c5e5e7f5ca760ce0e8b39efe9fb92c
Jihen.Bouguerra <boujihen647@gmail.com>
2020-11-11 12:29:33 +0100

    failed test

--- test/test_rest_api.py
+++ test/test_rest_api.py
@@ -6,6 +6,9 @@ class TestRestAPI(unittest.TestCase):
     def test_upper(self):
         self.assertEqual('foo'.upper(), 'FOO')
 
+    def test_upper_(self):
+        self.assertEqual('fo'.upper(), 'FOO')
+
 
 if __name__ == '__main__':
     unittest.main()
