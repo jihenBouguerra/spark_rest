3ed256fa65397c72468cce50f70ba487ddcb2d74
Jihen.Bouguerra <boujihen647@gmail.com>
2020-11-11 13:04:11 +0100

    run.bat

--- test/test_rest_api.py
+++ test/test_rest_api.py
@@ -6,9 +6,6 @@ class TestRestAPI(unittest.TestCase):
     def test_upper(self):
         self.assertEqual('foo'.upper(), 'FOO')
 
-    # def test_upper_(self):
-    #     self.assertEqual('fo'.upper(), 'FOO')
-
 
 if __name__ == '__main__':
     unittest.main()
