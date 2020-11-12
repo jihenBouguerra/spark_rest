2ac463bd5486618b661f84cf2cb7ca59a4ed7237
Jihen.Bouguerra <boujihen647@gmail.com>
2020-11-12 12:04:16 +0100

    pass test

--- test/api/test_rest_api.py
+++ test/api/test_rest_api.py
@@ -6,6 +6,6 @@ class TestRestAPI(unittest.TestCase):
 
     def test_upper(self):
         self.assertEqual('foo'.upper(), 'FOO')
-
-    def test_upper(self):
-        self.assertEqual('fo'.upper(), 'FOO')
+    #
+    # def test_upper(self):
+    #     self.assertEqual('fo'.upper(), 'FOO')
